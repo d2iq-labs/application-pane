2addc184a30eb85539eb1d1515f444c42a7dd646
Russell Anderson <randerson@d2iq.com>
2022-12-13 11:56:37 -0600

    feat: ingress on traefik (#3)
    Set up an ingress for traefik that will have this app running at the
    same place as the other dashboards

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { PageHeader, Flex } from "@d2iq/ui-kit";
 
 import styles from "./Header.module.css";
 
 export const Header = () => {
+  const { basePath } = useRouter();
   return (
     <PageHeader
       className={styles.header}
       breadcrumbElements={[
         <Flex key="header" align="center" gutterSize="m">
-          <Image src="/d2iq.svg" alt="D2iQ Logo" width={32} height={32} /> DKP
-          Application Pane
+          <Image
+            src={`${basePath}/d2iq.svg`}
+            alt="D2iQ Logo"
+            width={32}
+            height={32}
+          />{" "}
+          DKP Application Pane
         </Flex>,
       ]}
     ></PageHeader>
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
+  basePath: "/dkp/single-pane",
 };
 
 module.exports = nextConfig;
